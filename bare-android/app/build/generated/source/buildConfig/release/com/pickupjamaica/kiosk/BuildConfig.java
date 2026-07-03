1a73ad16365c8ffced90a779c5b711237daaae47
Z User <z@container>
2026-07-03 05:20:20 +0000

    0118b852-fc71-4a51-a955-d5be490e6fef

---
+++ bare-android/app/build/generated/source/buildConfig/release/com/pickupjamaica/kiosk/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pickupjamaica.kiosk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.pickupjamaica.kiosk";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "3.1";
+}
